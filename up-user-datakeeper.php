31ff0bb9218f52b96ce21e20a60b506c45cd624a
Lucas Viewup <lucas@viewup.com.br>
2017-05-19 10:46:51 -0300

    update docs

--- up-user-datakeeper.php
+++ up-user-datakeeper.php
@@ -1,27 +1,27 @@
 <?php
 /*
-Plugin Name: WordPress.org Plugin
-Plugin URI:  https://developer.wordpress.org/plugins/the-basics/
+Plugin Name: Up User Datakeeper
+Plugin URI:  https://developer.wordpress.org/plugins/up-user-datakeeper
 Description: Basic WordPress Plugin Header Comment
-Version:     20160911
+Version:     1.0.0
 Author:      WordPress.org
-Author URI:  https://developer.wordpress.org/
-Text Domain: wporg
+Author URI:  http://viewup.com.br/
+Text Domain: up-user-datakeeper
 Domain Path: /languages
 License:     GPL2
  
-{Plugin Name} is free software: you can redistribute it and/or modify
+Up User Datakeeper is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 any later version.
  
-{Plugin Name} is distributed in the hope that it will be useful,
+Up User Datakeeper is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
  
 You should have received a copy of the GNU General Public License
-along with {Plugin Name}. If not, see {License URI}.
+along with Up User Datakeeper. If not, see https://www.gnu.org/licenses/old-licenses/gpl-2.0.html.
 */
 
 class UpUserDatakeeper{
@@ -37,9 +37,16 @@ class UpUserDatakeeper{
 		});
 	}
 
+	function initUninstallHook(){
+		register_uninstall_hook(__FILE__, function(){
+
+		});
+	}
+
 	function __construct(){
 		$this->initActivationHook();
 		$this->initDeactivationHook();
+		$this->initUninstallHook();
 	}
 
 }
